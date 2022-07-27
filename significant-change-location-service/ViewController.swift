b8ac5383a4fc81fc2f3579f8f76cfc4ea600509e
phattarapon <phattarapon.cpe61346@gmail.com>
2022-07-27 15:25:47 +0700

    Initial Commit

---
+++ significant-change-location-service/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  significant-change-location-service
+//
+//  Created by phattarapon on 27/7/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
